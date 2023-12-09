664c0e20b7a2923fce48ae787fdc15a6963889b5
prabuddaFernando <prabuddafernando@gmail.com>
2023-12-09 13:54:04 +0100

    add android base folderstructure.

--- IOT-Project-2-5/EyeCare/app/src/main/java/com/dsv/iot25/MainActivity.kt
+++ IOT-Project-2-5/EyeCare/app/src/main/java/com/dsv/iot25/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.dsv.iot25
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttToken
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttException
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val SERVER_URI = "tcp://test.mosquitto.org:1883"
+    private val TAG = "MainActivity"
+
+    //    private static String[]  pub_topics = {"iotlab/Proximity/Sensors","iotlab/Lux/Sensors"};
+
+    private val pub_topics = listOf("iotlab/RGB/Sensors")
+
+    private var client: MqttAndroidClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
+
+    private fun connectToServer(){
+        CoroutineScope(IO).launch {
+            val clientId = MqttClient.generateClientId()
+            client = MqttAndroidClient(
+                this@MainActivity, SERVER_URI,
+                clientId
+            )
+            try {
+                val token = client?.connect()
+                token?.actionCallback = object : IMqttActionListener {
+                    override fun onSuccess(asyncActionToken: IMqttToken) {
+
+                        // We are connected
+                        Log.d(TAG, "onSuccess")
+                        println(TAG + " Success. Connected to " + SERVER_URI)
+                    }
+
+                    override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                        // Something went wrong e.g. connection timeout or firewall problems
+                        Log.d(TAG, "onFailure")
+                        println(
+                            TAG + " Oh no! Failed to connect to " +
+                                    SERVER_URI
+                        )
+                    }
+                }
+            } catch (e: MqttException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+
+
+
 }
